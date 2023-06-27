07c599d28791281cdc1db3f3b4f220e8f56d2186
Riyan Gonsalves <riyan0350@gmail.com>
2023-06-27 21:47:27 +0530

    added template engine & static directories

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+VIEW_ENGINE = 'ejs'
+VIEWS = './views'
+STATIC = ['public', 'images']
+
+app.set('view engine', VIEW_ENGINE)
+app.set('views', VIEWS)
+
+STATIC.forEach(directory => {
+    app.use(express.static(directory))
+});
+
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
