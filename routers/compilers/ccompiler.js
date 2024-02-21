b738562ae4e31e6c2f3402870d68d0e8d306e771
Riyan Gonsalves <riyan0350@gmail.com>
2024-02-21 18:34:09 +0530

    working c compiler, need to fix leftover files situation and exit at once when error

--- routers/compilers/ccompiler.js
+++ routers/compilers/ccompiler.js
@@ -1,47 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
-const Compiler = require("./compiler");
-const path = require("path");
+const bodyParser = require('body-parser');
+const Compiler = require('./compiler');
+const path = require('path');
 
-let COMPILE_COMMAND = (codeFile, exeFile) => {
-    return "gcc ./files/" + codeFile + " -o ./files/" + exeFile;
+let compileCommand = (codeFile, exeFile) => {
+    return 'gcc ./files/' + codeFile + ' -o ./files/' + exeFile;
 };
-let RUN_COMMAND = (exeFile, inputFile) => {
-    return "./files/" + exeFile + " < ./files/" + inputFile;
+let runCommand = (exeFile, inputFile) => {
+    return './files/' + exeFile + ' < ./files/' + inputFile;
 };
 
-class CCompiler extends Compiler {
-    constructor() {
-        super();
-    }
-}
-let ccompiler = new CCompiler();
+let compiler = new Compiler(compileCommand, runCommand, 'c');
 
 router.use(bodyParser.json());
 
-router.post("/", async (req, res) => {
-    let code = req.body.code;
-    let input = req.body.input;
+router.post('/runtrivial', async (req, res) => {
     try {
-        let files = await ccompiler.storeCode("c", code, input);
-        files.exeFile = files.codeFile.slice(
-            0,
-            files.codeFile.length - ".c".length
+        outputs = await compiler.runTrivial(
+            req.body.code,
+            req.body.testcases
         );
-
-        let output = await ccompiler.execute(
-            COMPILE_COMMAND(files.codeFile, files.exeFile),
-            RUN_COMMAND(files.exeFile, files.inputFile)
-        );
-        res.send(output);
-
-        ccompiler.removeFile(files.codeFile);
-        ccompiler.removeFile(files.exeFile);
-        ccompiler.removeFile(files.inputFile);
-    } catch (e) {
-        res.send(e);
-        console.log(e);
+        res.json({ outputs });
+    } catch (error) {
+        res.json({ error: error.message });
     }
 });
 
--- routers/compilers/compiler.js
+++ routers/compilers/compiler.js
@@ -1,15 +1,18 @@
-const { exec } = require("child_process");
-const AbstractCompiler = require("./abstractcompiler");
+const { exec } = require('child_process');
+const AbstractCompiler = require('./abstractcompiler');
 
 class Compiler extends AbstractCompiler {
-    constructor() {
+    constructor(compileCommand, runCommand, extension) {
         super();
+        this.compileCommand = compileCommand;
+        this.runCommand = runCommand;
+        this.extension = extension;
     }
 
     async execute(compileCommand, runCommand) {
         // code to compile the sent program
         let compilerError = await new Promise((resolve) => {
-            const process = exec(compileCommand, (error, stdout, stderr) => {
+            exec(compileCommand, (error, stdout, stderr) => {
                 if (error) resolve(error.message);
                 else {
                     resolve();
@@ -20,19 +23,49 @@ class Compiler extends AbstractCompiler {
         // if there is an error at compile time, there is no running the executable,
         // hence this compiler error is returned
         if (compilerError)
-            return new Promise((resolve) => resolve(compilerError));
+            return new Promise((resolve, reject) => reject(compilerError));
         // run the program if no compile time errors
         else
             return new Promise((resolve, reject) => {
                 // this process id is stored to be used for the timeout check
                 const process = exec(runCommand, (error, stdout, stderr) => {
                     if (error) reject(error.message);
+                    else if (stderr) reject(stderr);
                     else resolve(stdout);
                 });
 
                 // to avoid the execution getting stuck in a loop
                 this.timeoutCheck(process, reject);
             });
+    }
+
+    async runTrivial(code, testcases) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let codeFile = await this.storeCode(this.extension, code);
+                let exeFile = codeFile.slice(
+                    0,
+                    codeFile.length - '.c'.length
+                );
+
+                let outputs = [];
+                for (const testcase of testcases) {
+                    let inputFile = await this.storeInput(testcase);
+                    let output = await this.execute(
+                        this.compileCommand(codeFile, exeFile),
+                        this.runCommand(exeFile, inputFile)
+                    );
+                    outputs.push(output);
+                    this.removeFile(inputFile);
+                }
+                resolve(outputs);
+
+                this.removeFile(codeFile);
+                this.removeFile(exeFile);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 }
 
